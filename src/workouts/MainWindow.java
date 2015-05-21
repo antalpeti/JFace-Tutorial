6e2ea7cc6cc2c689d512fa31de2b523cf9948c98
Peti <antalpeti@users.noreply.github.com>
2015-05-21 09:42:54 +0200

    Added tooltips to the buttons. Tied up the Rename button. Restore previously browsed directory for the next browse. Enabled or disabled Preview button according to the file selection (.log, .txt).

--- src/workouts/MainWindow.java
+++ src/workouts/MainWindow.java
@@ -3,6 +3,7 @@ package workouts;
 import java.io.File;
 import java.util.ArrayList;
 
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
@@ -33,6 +34,7 @@ public class MainWindow {
   private Button btnDelete;
   private Button btnDuplicate;
   private Button btnPreview;
+  private String selectedDirectory;
 
   /**
    * Launch the application.
@@ -130,7 +132,10 @@ public class MainWindow {
           btnRename.setEnabled(true);
           btnDelete.setEnabled(true);
           btnDuplicate.setEnabled(true);
-          btnPreview.setEnabled(true);
+          if (((File) selection.getFirstElement()).getName().endsWith(".log")
+              || ((File) selection.getFirstElement()).getName().endsWith(".txt")) {
+            btnPreview.setEnabled(true);
+          }
         } else if (selection.size() == 0) {
           btnRename.setEnabled(false);
           btnDelete.setEnabled(false);
@@ -151,12 +156,17 @@ public class MainWindow {
     new Label(compositeBtn, SWT.NONE);
 
     Button btnBrowse = new Button(compositeBtn, SWT.NONE);
+    btnBrowse.setToolTipText("Browse a directory");
     btnBrowse.addSelectionListener(new SelectionAdapter() {
+
       @Override
       public void widgetSelected(SelectionEvent e) {
         String oldDir = textSelectedDirectory.getText();
         DirectoryDialog dlg = new DirectoryDialog(shlFileManipulator);
-        String selectedDirectory = dlg.open();
+        if (selectedDirectory != null) {
+          dlg.setFilterPath(selectedDirectory);
+        }
+        selectedDirectory = dlg.open();
         if (selectedDirectory != null && !selectedDirectory.equals(oldDir)) {
           textSelectedDirectory.setText(selectedDirectory);
           File dir = new File(selectedDirectory);
@@ -168,6 +178,7 @@ public class MainWindow {
           }
           listViewerFiles.setInput(files);
         }
+        listViewerFiles.refresh();
       }
     });
     btnBrowse.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
@@ -176,27 +187,45 @@ public class MainWindow {
     new Label(compositeBtn, SWT.NONE);
 
     btnRename = new Button(compositeBtn, SWT.NONE);
+    btnRename.setToolTipText("Rename the selected file");
+    btnRename.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        IStructuredSelection selection = (IStructuredSelection) listViewerFiles.getSelection();
+        File file = (File) selection.getFirstElement();
+        InputDialog dlg =
+            new InputDialog(Display.getCurrent().getActiveShell(), "Rename", "Rename file", file
+                .getName(), null);
+        dlg.open();
+        String newFileName = dlg.getValue();
+
+      }
+    });
     btnRename.setEnabled(false);
     btnRename.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     btnRename.setText("Rename");
 
     btnDelete = new Button(compositeBtn, SWT.NONE);
+    btnDelete.setToolTipText("Delete the selected file(s)");
     btnDelete.setEnabled(false);
     btnDelete.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     btnDelete.setText("Delete");
 
     btnDuplicate = new Button(compositeBtn, SWT.NONE);
+    btnDuplicate.setToolTipText("Duplicate the selected file");
     btnDuplicate.setEnabled(false);
     btnDuplicate.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     btnDuplicate.setText("Duplicate");
 
     btnPreview = new Button(compositeBtn, SWT.NONE);
+    btnPreview.setToolTipText("Preview the selected .log or .txt file");
     btnPreview.setEnabled(false);
     btnPreview.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     btnPreview.setText("Preview");
     new Label(compositeBtn, SWT.NONE);
 
     Button btnExit = new Button(compositeBtn, SWT.NONE);
+    btnExit.setToolTipText("Exit from the program");
     btnExit.addSelectionListener(new SelectionAdapter() {
       @Override
       public void widgetSelected(SelectionEvent e) {
